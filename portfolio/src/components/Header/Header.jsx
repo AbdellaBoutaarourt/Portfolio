41f0395353c1b0d43e0d30c1488cc767824cbd54
Abdella Boutaarourt <abdella.boutaarourt@student.ehb.be>
2024-12-07 17:14:42 +0100

    Header

---
+++ portfolio/src/components/Header/Header.jsx
@@ -0,0 +1,60 @@
+import "./header.css";
+import { BrowserRouter as Router } from "react-router-dom";
+import { NavHashLink, HashLink } from "react-router-hash-link";
+import { useState } from "react";
+import Resume from "../../cv.pdf";
+
+export function Header() {
+  const [isActive, setActive] = useState(false);
+  function toggleTheme() {
+    let html = document.getElementsByTagName("html")[0];
+    html.classList.toggle("light");
+  }
+  function closeMenu() {
+    setActive(false);
+  }
+  return (
+    <header className="header-fixed">
+      <Router>
+        <HashLink smooth to="#home" className="logo">
+          <span>{"<Abdel "}</span>
+          <span>{" Bt/>"}</span>
+        </HashLink>
+        <input
+          onChange={toggleTheme}
+          className="container_toggle"
+          type="checkbox"
+          id="switch"
+          name="mode"
+        />
+        <label htmlFor="switch"></label>
+        <nav className={isActive ? "active" : ""}>
+          <NavHashLink smooth to="#home" onClick={closeMenu}>
+            Home
+          </NavHashLink>
+          <NavHashLink smooth to="#about" onClick={closeMenu}>
+            About me
+          </NavHashLink>
+          <NavHashLink smooth to="#project" onClick={closeMenu}>
+            Project
+          </NavHashLink>
+          <NavHashLink smooth to="#contact" onClick={closeMenu}>
+            Contact
+          </NavHashLink>
+          <a href={Resume} download className="button">
+            Resume
+          </a>
+        </nav>
+        <div
+          aria-expanded={isActive ? "true" : "false"}
+          aria-haspopup="true"
+          aria-label={isActive ? "Fechar menu" : "Abrir menu"}
+          className={isActive ? "menu active" : "menu"}
+          onClick={() => {
+            setActive(!isActive);
+          }}
+        ></div>
+      </Router>
+    </header>
+  );
+}
